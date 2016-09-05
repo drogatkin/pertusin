1e032f793d8cab07445bf8e0301046fa2de44246
dmitriy <dmitriy@home>
2016-09-05 13:35:01 -0700

    scaling added

--- src/java/rogatkin/mobile/data/pertusin/BarcodeAssistant.java
+++ src/java/rogatkin/mobile/data/pertusin/BarcodeAssistant.java
@@ -2,6 +2,7 @@ package rogatkin.mobile.data.pertusin;
 
 import java.io.OutputStream;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import io.nayuki.qrcodegen.QrCode;
@@ -16,8 +17,21 @@ public class BarcodeAssistant {
 	public Bitmap getQR(String text) {
 		return getQR(text, 10, 4, Ecc.MEDIUM);
 	}
+	
+	public Bitmap getQR(String text, Context context) {
+		return getQR(text, getScale(context), 4, Ecc.MEDIUM);
+	}
 
 	public Bitmap getQR(String text, int scale, int border, Ecc erc) {
 		return QrCode.encodeText(text, erc).toImage(scale, border);
+	}
+	
+	public static int getScale(Context context) {
+		int dpi = context.getResources().getDisplayMetrics().densityDpi;
+		if (dpi > 520)
+			return 16;
+		else if (dpi > 410)
+			return 12;
+		return 10;
 	}
 }
